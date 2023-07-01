35761f2de0bf4532caae4c9dc5c85ba436cf2733
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2023-07-01 20:24:40 +0300

    Delete Basics of programming for Linux/Introduction to Development for Linux/task1/task1 directory

--- Basics of programming for Linux/Introduction to Development for Linux/task1/task1/source/solution.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STRING_STAT__
-#define __STRING_STAT__
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
-
-int stringStat(const char* string, size_t multiplier, int* count);
-
-#endif 
