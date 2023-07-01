af5da31af15195269e2ed3506358900eabe11896
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2023-07-01 20:23:51 +0300

    Add files via upload

---
+++ Basics of programming for Linux/Introduction to Development for Linux/task1/task1/source/solution.h
@@ -0,0 +1,9 @@
+#ifndef __STRING_STAT__
+#define __STRING_STAT__
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+int stringStat(const char* string, size_t multiplier, int* count);
+
+#endif 
