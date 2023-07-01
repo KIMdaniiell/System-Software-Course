ae2985fdf80c025aef4ff81495825cb19f9d3319
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2023-07-01 20:13:38 +0300

    Update solution.c

--- Basics of programming for Linux/Introduction to Development for Linux/task1/source/solution.c
+++ Basics of programming for Linux/Introduction to Development for Linux/task1/source/solution.c
@@ -2,13 +2,13 @@
 
 int stringStat(const char* string, size_t multiplier, int* count) {
 	size_t result = strlen(string);
-	printf("Длина строки: %zd\n", result);
+	//printf("Длина строки: %zd\n", result);
 
 	result *= multiplier;
-	printf("Длина строки * множитель: %zd\n", result);
+	//printf("Длина строки * множитель: %zd\n", result);
 
 	result += *count;
-	printf("Длина строки * множитель + число: %zd\n",result);
+	//printf("Длина строки * множитель + число: %zd\n",result);
 
 	return result;
 }
