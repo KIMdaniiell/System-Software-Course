6449e9438d63c319f9a205655a31588d779dca6c
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2023-07-08 00:01:34 +0300

    Add files via upload

--- Basics of programming for Linux/Inter-process communication/task5/solution.c
+++ Basics of programming for Linux/Inter-process communication/task5/solution.c
@@ -11,32 +11,27 @@ int main(int argc, char* argv[]) {
 	if (argc != 3)
 		return 1;
 	
-	int shmid1 = atoi(argv[1]);
-	int shmid2 = atoi(argv[2]);
-	//printf("%d %d\n", shmid1, shmid2);
-
-	int * reg_ptr1 = NULL;
-	int * reg_ptr2 = NULL;
-	int * reg_ptr3 = NULL;
-
-	shmat(shmid1, reg_ptr1, SHM_RDONLY);
-	shmat(shmid2, reg_ptr2, SHM_RDONLY);
-	
+	int shmkey1 = atoi(argv[1]);
+	int shmkey2 = atoi(argv[2]);
 	key_t key = ftok(argv[0], 0);
+	printf("%d\n", key);
+
+	int shmid1 = shmget(shmkey1, REG_SIZE, 0);
+	int shmid2 = shmget(shmkey2, REG_SIZE, 0);
 	int shmid3 = shmget(key, REG_SIZE, 0666|IPC_CREAT);
-	shmat(shmid3, reg_ptr3, 0);
+	int * reg_ptr1 = shmat(shmid1, NULL, SHM_RDONLY);
+	int * reg_ptr2 = shmat(shmid2, NULL, SHM_RDONLY);
+	int * reg_ptr = shmat(shmid3, NULL, 0);
 
 	int i;
 	for (i = 0; i < LIST_SIZE; i++) {
-		reg_ptr3[i] = reg_ptr1[i] + reg_ptr2[2];
+		reg_ptr[i] = reg_ptr1[i] + reg_ptr2[2];
 	}
 
 	shmdt(reg_ptr1);
 	shmdt(reg_ptr2);
-	shmdt(reg_ptr3);
-
-
-	printf("%d\n", key);
+	shmdt(reg_ptr);
+	
 	return 0;
 
 }
